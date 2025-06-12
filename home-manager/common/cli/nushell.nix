713ce1be2d9c36ca77bc94c739bb920b6e5b12f7
sky-danielsantos <daniel.santos@sky.uk>
2025-06-12 16:28:16 +0100

    feat: add nushell

--- home-manager/common/cli/nushell.nix
+++ home-manager/common/cli/nushell.nix
@@ -4,11 +4,44 @@
     nushell = {
       enable = true;
 
+      shellAliases = {
+        grcm = "git reset --hard HEAD; try { git checkout master } catch { git checkout main }; git fetch --all; git pull; try { git reset --hard origin/master } catch { git reset --hard origin/main }";
+        v = "nvim";
+        vim = "nvim";
+      };
+
       plugins = with pkgs; [
         nushellPlugins.formats
         nushellPlugins.query
         nushellPlugins.net
       ];
+
+      extraConfig =
+        let
+          conf = builtins.toJSON
+            {
+              show_banner = false;
+              edit_mode = "vi";
+
+              ls.clickable_links = true;
+
+              cursor_shape = {
+                vi_insert = "line";
+                vi_normal = "block";
+
+              };
+
+            };
+        in
+        ''
+          $env.config = ${conf};
+
+          def yv [] {
+            to yaml | bat --paging=always --language=yaml
+          }
+
+        '';
+
     };
 
     atuin.enableNushellIntegration = true;
